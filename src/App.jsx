80d6a22dd7f8d4ae0ceeb7a1240592d64e1aff11
shoaibmhr <shoaibmhr895@gmail.com>
2026-06-30 12:35:09 +0500

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-5xl font-bold text-white">Tailwind CSS Working ✅</h1>
+      <h2 className="text-5xl font-bold text-white">Tailwind CSS Working ✅</h2>
+      <h3 className="text-2xl"><Shoaib></Shoaib></h3>
+
     </div>
   );
 }
