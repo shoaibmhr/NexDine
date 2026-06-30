7d2b08787a3d788b5ac6924ba76d31e30e05da75
huzaifaseo2704-sketch <huzaifaseo2704@gmail.com>
2026-06-30 12:36:41 +0500

    hahaha

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div className="min-h-screen flex items-center justify-center bg-blue-500">
       <h2 className="text-5xl font-bold text-white">Tailwind CSS Working ✅</h2>
       <h3 className="text-2xl"><Shoaib></Shoaib></h3>
+      <h2 className="text-3xl">Huzaifa</h2>
 
     </div>
   );
