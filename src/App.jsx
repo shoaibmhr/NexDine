a6bce951b3a1e1f3d3e872eaf5cd9ef714226e6a
shoaibmhr <shoaibmhr895@gmail.com>
2026-06-30 12:02:50 +0500

    Initial Project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-5xl font-bold text-white">Tailwind CSS Working ✅</h1>
+    </div>
+  );
+}
+
+export default App;
