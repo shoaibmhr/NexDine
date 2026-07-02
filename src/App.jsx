5936fc0f850d40f7d795cf977949ddd834e2ae55
shoaibmhr <shoaibmhr895@gmail.com>
2026-07-02 10:55:16 +0500

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-500">
       <h2 className="text-5xl font-bold text-white">Tailwind CSS Working ✅</h2>
-      <h3 className="text-2xl"><Shoaib></Shoaib></h3>
+      <h3 className="text-2xl">shoaib</h3>
       <h2 className="text-3xl">Huzaifa</h2>
 
     </div>
