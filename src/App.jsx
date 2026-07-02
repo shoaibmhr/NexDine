758c9b8d2dc483b3115e68fcb030a10889018dab
huzaifaseo2704-sketch <huzaifaseo2704@gmail.com>
2026-07-02 11:57:05 +0500

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import MainLayout from "./layouts/MainLayout";
+import React from 'react'
 
-function App() {
-  return <MainLayout />;
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h2 className="text-5xl font-bold text-white">Tailwind CSS Working ✅</h2>
+      <h3 className="text-2xl">shoaib</h3>
+      <h2 className="text-3xl">Huzaifa</h2>
+
+    </div>
+  )
 }
-
-export default App;
